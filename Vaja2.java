83f6000877e6fa49456b826e9669a3da6db78936
mojcap <47428004+mojcap@users.noreply.github.com>
2019-02-07 19:02:32 +0100

    Add files via upload

---
+++ Vaja2.java
@@ -0,0 +1,18 @@
+public class Vaja2 {
+
+  public static void main(String[] args) {
+
+    int stevilo = 10; // deklariramo celo število 10
+
+    System.out.println("Stevilo je " + stevilo);
+
+    // komentar kode
+
+    /*
+še en komentar
+tokrat čez
+več
+vrstic
+    */
+  }
+}
